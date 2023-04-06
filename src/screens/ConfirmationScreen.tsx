5fc42f9d44ecdd9d2a9c8657ff5036e341fe2042
Rachael Yoder <54332655+rachyoder@users.noreply.github.com>
2023-04-06 16:30:22 -0400

    Add confirmation text to page
    Added confirmation text and styling to main page

--- src/screens/ConfirmationScreen.tsx
+++ src/screens/ConfirmationScreen.tsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { ImageBackground, SafeAreaView, View } from "react-native";
-import ListTile from "../components/ListTile";
+import { ImageBackground, SafeAreaView, View, Text, StyleSheet } from "react-native";
 import CloseButton from '../components/CloseButton'
 
 const ConfirmationScreen = () => {
@@ -9,11 +8,58 @@ const ConfirmationScreen = () => {
             <ImageBackground source={require("../../assets/images/bg-map.png")} resizeMode="cover" style={{ height: "100%", width: "100%" }}>
                 <SafeAreaView>
                     <CloseButton />
+                    <ConfirmationText />
+                    <LabelText />
                 </SafeAreaView>
     
             </ImageBackground>
         </View>
     );
 };
+
+const ConfirmationText = () => {
+    return(
+        <View style={styles.header_container}>
+            <Text style={styles.header}>
+                Nice! Your monthly parking is confirmed.
+            </Text>
+        </View>
+    );
+};
+
+const LabelText = () => {
+    return(
+        <View style={styles.label_container}>
+            <Text style={styles.label}>
+                Your reservation details are below.
+            </Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    header: {
+        fontFamily: "Rubik-Regular",
+        fontWeight: "900",
+        fontSize: 20,
+        textTransform: "uppercase",
+        lineHeight: 24,
+    },
+    header_container: {
+        marginTop: 34,
+        marginLeft: 24,
+        width: 311
+    },
+    label: {
+        fontFamily: "Rubik-Regular",
+        fontWeight: "400",
+        fontSize: 13,
+        lineHeight: 18,
+    },
+    label_container: {
+        marginTop: 8,
+        marginLeft: 24,
+    },
+});
 
 export default ConfirmationScreen;
